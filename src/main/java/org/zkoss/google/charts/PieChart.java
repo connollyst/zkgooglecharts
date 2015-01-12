f31f2f176c1ce18294b9b6618798b9ffe744294d
Sean Connolly <connolly.st@gmail.com>
2015-01-12 22:34:00 +0100

    Working example with hard coded data coming from the server.

--- src/main/java/org/zkoss/google/charts/PieChart.java
+++ src/main/java/org/zkoss/google/charts/PieChart.java
@@ -1,6 +1,10 @@
 package org.zkoss.google.charts;
 
-import org.zkoss.zk.ui.event.Events;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.zkoss.zk.ui.sys.ContentRenderer;
 import org.zkoss.zul.impl.XulElement;
 
 /**
@@ -8,8 +12,46 @@ import org.zkoss.zul.impl.XulElement;
  */
 public class PieChart extends XulElement {
 
-	public void drawChart() {
-		Events.sendEvent(this, new DrawEvent(this, "Hi"));
+	private Map<String, Map<String, Object>> data = new HashMap<String, Map<String, Object>>();
+	private Map<String, String> options = new HashMap<String, String>();
+
+	public PieChart() {
+		data.put("columns", new HashMap<String, Object>());
+		data.put("rows", new HashMap<String, Object>());
+		data.get("columns").put("string", "Filling");
+		data.get("columns").put("number", "Pieces");
+		data.get("rows").put("Custard", 20);
+		data.get("rows").put("Chocolate", 50);
+		data.get("rows").put("Meat", 3);
+		options.put("title", "Pudding");
+		options.put("width", "800");
+		options.put("height", "600");
+	}
+
+	public String getData() {
+		return data.toString();
+	}
+
+	public void setData(String data) {
+		// this.data = data;
+		smartUpdate("chartData", this.data);
+	}
+
+	public String getOptions() {
+		return options.toString();
+	}
+
+	public void setOptions(String options) {
+		// this.options = options;
+		smartUpdate("chartOptions", this.options);
+	}
+
+	@Override
+	protected void renderProperties(ContentRenderer renderer)
+			throws IOException {
+		super.renderProperties(renderer);
+		render(renderer, "chartData", data);
+		render(renderer, "chartOptions", options);
 	}
 
 }
--- src/main/resources/web/js/zul/googlecharts/PieChart.js
+++ src/main/resources/web/js/zul/googlecharts/PieChart.js
@@ -1,43 +1,62 @@
 zul.googlecharts.PieChart = zk.$extends(zk.Widget, {
 
-    doDrawChart: function () {
+    _chartData: '',
+    _chartOptions: '',
+
+    $define: {
+        chartData: [
+            function (data) {
+                return data; // TODO this shouldn't be necessary!?
+            },
+            function () {
+                console.log('Setting data: ' + this._chartData);
+                // TODO if options exists, redraw
+                if (this.desktop) this.rerender();
+            }
+        ],
+        chartOptions: [
+            function (options) {
+                return options; // TODO this shouldn't be necessary!?
+            },
+            function () {
+                console.log('Setting options: ' + this._chartOptions);
+                // TODO if data exists, redraw
+                if (this.desktop) this.rerender();
+            }
+        ]
+    },
+
+    // protected //
+
+    doDrawChart_: function () {
         console.log("Drawing..");
+        console.log(this._chartData);
+        console.log(this._chartOptions);
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-            ['Mushrooms', 3],
-            ['Onions', 1],
-            ['Olives', 1],
-            ['Zucchini', 1],
-            ['Pepperoni', 2]
-        ]);
-        var options = {
-            'title': 'How Much Pizza I Ate Last Night',
-            'width': 400,
-            'height': 300
-        };
+        for (var type in this._chartData['columns']) {
+            var label = this._chartData['columns'][type];
+            console.log(type + " = " + label);
+            data.addColumn(type, label);
+        }
+        for (var key in this._chartData['rows']) {
+            var value = this._chartData['rows'][key];
+            data.addRow([key, value]);
+        }
         // Instantiate and draw our chart, passing in some options.
-        console.log(this);
         console.log('Rendering pie chart into ' + this.uuid);
         var container = document.getElementById(this.uuid);
-        console.log('Rendering pie chart into ' + container);
         var chart = new google.visualization.PieChart(container);
-        chart.draw(data, options);
+        chart.draw(data, this._chartOptions);
     },
-    // protected //
     bind_: function () {
-        console.log('Binding widget..');
         this.$supers(zul.googlecharts.PieChart, 'bind_', arguments);
-        // Set a callback to run when the Google Visualization API is loaded.
         var self = this;
         google.setOnLoadCallback(function () {
-            console.log('Google Charts loaded..');
-            self.doDrawChart();
+            self.doDrawChart_();
         });
     },
     unbind_: function () {
         this.$supers(zul.googlecharts.PieChart, 'unbind_', arguments);
     }
 
-}, {});
+});
