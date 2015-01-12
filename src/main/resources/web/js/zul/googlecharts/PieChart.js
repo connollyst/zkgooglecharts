a4fe2e55b8a2bc138d19e3e082495421cab3e9cc
Sean Connolly <connolly.st@gmail.com>
2015-01-12 11:14:27 +0100

    Corrected widget package & name.

--- src/main/resources/web/js/zul/googlecharts/PieChart.js
+++ src/main/resources/web/js/zul/googlecharts/PieChart.js
@@ -2,10 +2,10 @@ zul.googlecharts.PieChart = zk.$extends(zk.Widget, {
 
     // protected //
     bind_: function () {
-        this.$supers(zul.fontawesome.Icon, 'bind_', arguments);
+        this.$supers(zul.googlecharts.PieChart, 'bind_', arguments);
     },
     unbind_: function () {
-        this.$supers(zul.fontawesome.Icon, 'unbind_', arguments);
+        this.$supers(zul.googlecharts.PieChart, 'unbind_', arguments);
     }
 
 }, {});
