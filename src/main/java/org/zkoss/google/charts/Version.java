28ec7e1b4b110c72584d315d4b82a305e3db7233
Sean Connolly <connolly.st@gmail.com>
2015-01-12 11:14:49 +0100

    Added addon Version

---
+++ src/main/java/org/zkoss/google/charts/Version.java
@@ -0,0 +1,13 @@
+package org.zkoss.google.charts;
+
+/**
+ * The version of the language addon.<br/>
+ * The value of {@code UID} here must match that in {@code lang-addon.xml}.
+ *
+ * @author Sean Connolly
+ */
+public class Version {
+
+	public static final String UID = "0.1";
+
+}
