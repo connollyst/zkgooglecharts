62e36b30a0863b62d0df699c5071a6ddc3459208
Sean Connolly <connolly.st@gmail.com>
2015-01-19 11:58:05 +0100

    Removed default data models.

--- src/main/java/org/zkoss/google/charts/PieChart.java
+++ src/main/java/org/zkoss/google/charts/PieChart.java
@@ -1,7 +1,5 @@
 package org.zkoss.google.charts;
 
-import org.zkoss.google.charts.data.FormattedValue;
-
 /**
  * <p>
  * Wrapper of the <a href=
@@ -12,18 +10,5 @@ import org.zkoss.google.charts.data.FormattedValue;
  * @author Sean Connolly
  */
 public class PieChart extends AbstractChart {
-
-	public PieChart() {
-//		data.addStringColumn("Task", "task");
-//		data.addNumberColumn("Hours per Day", "hours");
-//		data.addRow("Work", 11);
-//		data.addRow("Eat", 2);
-//		data.addRow("Commute", 2);
-//		data.addRow("Watch TV", 2);
-//		data.addRow("Sleep", new FormattedValue(7, "7.000"));
-//		options.put("title", "Time");
-//		options.put("width", "800");
-//		options.put("height", "600");
-	}
 
 }
