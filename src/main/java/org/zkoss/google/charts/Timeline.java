58462c682dcf4c8e613d959558a98f845afb6804
Sean Connolly <connolly.st@gmail.com>
2015-02-10 14:18:33 +0100

    Corrected ClassCastException setting Timeline parameters.

--- src/main/java/org/zkoss/google/charts/Timeline.java
+++ src/main/java/org/zkoss/google/charts/Timeline.java
@@ -1,6 +1,6 @@
 package org.zkoss.google.charts;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,74 +13,75 @@ import java.util.Map;
  */
 public class Timeline extends GoogleChart {
 
-    private static final String AVOID_OVERLAPPING_GRID_LINES = "avoidOverlappingGridLines";
-    private static final String COLORS = "colors";
-    private static final String ENABLE_INTERACTIVITY = "enableInteractivity";
-    private static final String TIMELINE = "timeline";
-    private static final String ROW_LABEL_STYLE = "rowLabelStyle";
-    private static final String BAR_LABEL_STYLE = "barLabelStyle";
-    private static final String COLOR_BY_ROW_LABEL = "colorByRowLabel";
-    private static final String GROUP_BY_ROW_LABEL = "groupByRowLabel";
-    private static final String SHOW_BAR_LABELS = "showBarLabels";
-    private static final String SHOW_ROW_LABELS = "showRowLabels";
-    private static final String SINGLE_COLOR = "singleColor";
+	private static final String AVOID_OVERLAPPING_GRID_LINES = "avoidOverlappingGridLines";
+	private static final String COLORS = "colors";
+	private static final String ENABLE_INTERACTIVITY = "enableInteractivity";
+	private static final String TIMELINE = "timeline";
+	private static final String ROW_LABEL_STYLE = "rowLabelStyle";
+	private static final String BAR_LABEL_STYLE = "barLabelStyle";
+	private static final String COLOR_BY_ROW_LABEL = "colorByRowLabel";
+	private static final String GROUP_BY_ROW_LABEL = "groupByRowLabel";
+	private static final String SHOW_BAR_LABELS = "showBarLabels";
+	private static final String SHOW_ROW_LABELS = "showRowLabels";
+	private static final String SINGLE_COLOR = "singleColor";
 
-    public String[] getColors() {
-        return getOption(COLORS, new String[0], String[].class);
-    }
+	public String[] getColors() {
+		return getOption(COLORS, new String[0], String[].class);
+	}
 
-    public void setColors(String... colors) {
-        setOption(COLORS, colors);
-    }
+	public void setColors(String... colors) {
+		setOption(COLORS, colors);
+	}
 
-    public void setEnableInteractivity(boolean interactive) {
-        setOption(ENABLE_INTERACTIVITY, interactive);
-    }
+	public void setEnableInteractivity(boolean interactive) {
+		setOption(ENABLE_INTERACTIVITY, interactive);
+	}
 
-    public void setAvoidOverlappingGridLines(boolean avoid) {
-        setOption(AVOID_OVERLAPPING_GRID_LINES, avoid);
-    }
+	public void setAvoidOverlappingGridLines(boolean avoid) {
+		setOption(AVOID_OVERLAPPING_GRID_LINES, avoid);
+	}
 
-    public void setRowLabelStyle(Map<String, String> css) {
-        setTimelineOption(ROW_LABEL_STYLE, css);
-    }
+	public void setRowLabelStyle(Map<String, String> css) {
+		setTimelineOption(ROW_LABEL_STYLE, css);
+	}
 
-    public void setBarLabelStyle(Map<String, String> css) {
-        setTimelineOption(BAR_LABEL_STYLE, css);
-    }
+	public void setBarLabelStyle(Map<String, String> css) {
+		setTimelineOption(BAR_LABEL_STYLE, css);
+	}
 
-    public void setColorByRowLabel(boolean color) {
-        setTimelineOption(COLOR_BY_ROW_LABEL, color);
-    }
+	public void setColorByRowLabel(boolean color) {
+		setTimelineOption(COLOR_BY_ROW_LABEL, color);
+	}
 
-    public void setGroupByRowLabel(boolean group) {
-        setTimelineOption(GROUP_BY_ROW_LABEL, group);
-    }
+	public void setGroupByRowLabel(boolean group) {
+		setTimelineOption(GROUP_BY_ROW_LABEL, group);
+	}
 
-    public void setShowBarLabels(boolean show) {
-        setTimelineOption(SHOW_BAR_LABELS, show);
-    }
+	public void setShowBarLabels(boolean show) {
+		setTimelineOption(SHOW_BAR_LABELS, show);
+	}
 
-    public void setShowRowLabels(boolean show) {
-        setTimelineOption(SHOW_ROW_LABELS, show);
-    }
+	public void setShowRowLabels(boolean show) {
+		setTimelineOption(SHOW_ROW_LABELS, show);
+	}
 
-    public void setSingleColor(String color) {
-        setTimelineOption(SINGLE_COLOR, color);
-    }
+	public void setSingleColor(String color) {
+		setTimelineOption(SINGLE_COLOR, color);
+	}
 
-    private Map<String, Object> getTimeline() {
-        // TODO avoid unchecked assignment here
-        return getOption(TIMELINE, Collections.emptyMap(), Map.class);
-    }
+	private Map<String, Object> getTimeline() {
+		@SuppressWarnings("unchecked")
+		Class<Map<String, Object>> clazz = (Class<Map<String, Object>>)(Class<?>)Map.class;
+		return getOption(TIMELINE, new HashMap<String, Object>(), clazz);
+	}
 
-    private void setTimeline(Map<String, Object> timeline) {
-        setOption(TIMELINE, timeline);
-    }
+	private void setTimeline(Map<String, Object> timeline) {
+		setOption(TIMELINE, timeline);
+	}
 
-    private void setTimelineOption(String name, Object value) {
-        Map<String, Object> timeline = getTimeline();
-        timeline.put(name, value);
-        setTimeline(timeline);
-    }
+	private void setTimelineOption(String name, Object value) {
+		Map<String, Object> timeline = getTimeline();
+		timeline.put(name, value);
+		setTimeline(timeline);
+	}
 }
